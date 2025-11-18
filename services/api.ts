3f2a907f8cbdc0827deb9010679b62f04d881f51
pedroServfaz <pedro.servfaz@gmail.com>
2025-11-18 12:42:56 -0300

    feat: adiciona CORS para Vercel

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333",
   withCredentials: true,
 });
